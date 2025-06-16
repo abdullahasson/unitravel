cee490e9a4405d862fcbaef53451ad84375939ae
abdullahasson <bh532997@gmail.com>
2025-06-16 16:26:17 +0300

    Confirm website ownership

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,8 @@
 // Next js
 import type { Metadata } from "next";
+import Script from 'next/script';
 // Google Fonts
-import { Cairo , Roboto } from "next/font/google";
+import { Cairo, Roboto } from "next/font/google";
 // Next Intl
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
@@ -37,22 +38,41 @@ export default async function RootLayout({
   params: Promise<{ locale: string }>;
 }>) {
 
-    // Ensure that the incoming `locale` is valid
-    const {locale} = await params;
-    if (!routing.locales.includes(locale as ValidLocale)) {
-      notFound();
-    }
-   
-    // Providing all messages to the client
-    // side is the easiest way to get started
-    const messages = await getMessages();
+  // Ensure that the incoming `locale` is valid
+  const { locale } = await params;
+  if (!routing.locales.includes(locale as ValidLocale)) {
+    notFound();
+  }
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
 
   return (
-    <html 
-      lang={locale} 
+    <html
+      lang={locale}
       data-theme='dark'
       className={`${roboto.className} ${cairo.className}`}
     >
+      <head>
+        <Script
+          id="external-script-loader"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                var script = document.createElement('script');
+                script.async = true;
+                script.src = 'https://emrldtp.com/NDI2NTc5.js?t=426579';
+                document.head.appendChild(script);
+              })();
+            `,
+          }}
+          data-noptimize="1"
+          data-cfasync="false"
+          data-wpfc-render="false"
+        />
+      </head>
       <body
         dir={locale == "ar" ? "rtl" : "ltr"}
         className={`antialiased`}
